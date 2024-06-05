def8f3b1e101864a84817782bbf1c61029af3c1d
Alberto Fabbri <alberto_fabbri.git@yahoo.com>
2024-06-05 17:55:46 +0200

    Code refactoring

--- Analysis/Utils.R
+++ Analysis/Utils.R
@@ -1,3 +1,8 @@
+# Import environment variables as global variables
+rds_dir <- Sys.getenv("HTTP_PROXY")
+image_dir <- Sys.getenv("IMAGE_DIR")
+image_ext <- Sys.getenv("IMAGE_EXT")
+
 ####### GENERATE HEATMAP #######
 
 # Heatmap plot, define a function to dynamically calculate label size based on the number of features
@@ -76,31 +81,13 @@ generateDynTextHeatmap <- function(data, clusters_column_name) {
 
 ####### INSITUTYPE SEMISUPERVISED #######
 
-runInSituTypeSemisupervised <- function(patient_data, ioprofiles) {
-
-  # Cohort of all the patients
-  patient_immunofluorescence <- patient_data@meta.data %>% select("Mean.PanCK", "Mean.CD45", "Mean.CD68")
-  # "Gaussian_transform = TRUE" maps variables to gaussians in order to place dramatically different variables on the same scale
-  patient_cohort <- fastCohorting(patient_immunofluorescence, gaussian_transform = TRUE, n_cohorts = 5)
-  # check clusters and cohort numbers
-  table(patient_cohort)
-  
-  # Extract the count data from the Seurat object
-  patient_rna.counts <- GetAssayData(subset(patient_data, features = row.names(GetAssayData(patient_data))[1:1000]), layer = "counts") %>%
-    as.matrix() %>%
-    t()
-  # Extract the negative probes from the Seurat object
-  patient_neg.probes <- GetAssayData(subset(patient_data, features = row.names(GetAssayData(patient_data)) %>% grep("Negative", ., value = TRUE))) %>%
-    as.matrix() %>%
-    t()
-  # Calculate the average negative probes per cell
-  patient_avg.neg.probes <- Matrix::rowMeans(patient_neg.probes)
+runInSituTypeSemisupervised <- function(patient_data, ioprofiles, patient_cohort, patient_rna_counts, patient_avg_neg_probes) {
   
   # Semi-supervised learning with insitutype and reference profiles
   # InSituType needs integers, if given floating point numbers it fails with misleading errors
   patient_semisup <- insitutype(
-    x = patient_rna.counts,
-    neg = patient_avg.neg.probes,
+    x = patient_rna_counts,
+    neg = patient_avg_neg_probes,
     cohort = patient_cohort,
     reference_profiles = ioprofiles,
     
@@ -125,4 +112,318 @@ runInSituTypeSemisupervised <- function(patient_data, ioprofiles) {
   )
   
   return(patient_semisup)
+}
+
+####### EXTRACT PATIENT DATA #######
+
+extract_patient_data <- function(all_patients_data, patient_num) {
+  
+  # Extract patient data from global Seurat object
+  patient_data <- subset(x = all_patients_data, subset = Patient.ID == patient_num)
+  
+  # Print cores and fovs associated with the patient
+  print(paste("FOVs and cell count associated with patient", patient_num))
+  patient_cores <- sort(unique(patient_data@meta.data$core_serial))
+  for(core in patient_cores) {
+    print(paste("----------", "CORE", core, "----------"))
+    patient_core_stamps <- sort(unique(patient_data@meta.data$stamp[patient_data@meta.data$core_serial == core]))
+    for(stamp in patient_core_stamps) {
+      print(paste("-----", "STAMP", stamp, "-----"))
+      print(table(patient_data@meta.data$fov[patient_data@meta.data$core_serial == core &  patient_data@meta.data$stamp == stamp]))
+    }
+  }
+  
+  # Create Seurat object with only the RNA data
+  patient_rna_only <- subset(x = patient_data, features = rownames(patient_data)[1:1000])
+  
+  # Cohort of the patient
+  patient_immunofluorescence <- patient_data@meta.data %>% select("Mean.PanCK", "Mean.CD45", "Mean.CD68")
+  # "Gaussian_transform = TRUE" maps variables to gaussians in order to place dramatically different variables on the same scale
+  patient_cohort <- fastCohorting(patient_immunofluorescence, gaussian_transform = TRUE, n_cohorts = 5)
+  # check clusters and cohort numbers
+  table(patient_cohort)
+  
+  # Extract the count data from the Seurat object
+  patient_rna_counts <- GetAssayData(
+    patient_rna_only,
+    layer = "counts") %>%
+  as.matrix() %>%
+  t()
+  # Extract the negative probes from the Seurat object
+  patient_neg_probes <- GetAssayData(
+      subset(
+        patient_data,
+        features = row.names(GetAssayData(patient_data)) %>%
+  grep("Negative", ., value = TRUE))) %>%
+  as.matrix() %>%
+  t()
+  # Calculate the average negative probes per cell
+  patient_avg_neg_probes <- Matrix::rowMeans(patient_neg_probes)
+  
+  return(list(patient_rna_only, patient_cohort, patient_rna_counts, patient_avg_neg_probes))
+}
+
+####### NORMALIZE, CLUSTER, ... PATIENT DATA #######
+
+normalize_cluster_data <- function(patient_rna_only, patient_dims, patient_res) {
+  
+  # Normalize the count data present in a given assay
+  patient_rna_only <- Seurat::NormalizeData(patient_rna_only, assay = "Nanostring")
+  # Scales and centers features in the dataset
+  patient_rna_only <- Seurat::ScaleData(patient_rna_only)
+  # Detect highly variable genes for the pca
+  # Identifies features that are outliers on a 'mean variability plot'
+  patient_rna_only <- Seurat::FindVariableFeatures(patient_rna_only)
+  # Run a PCA dimensionality reduction
+  patient_rna_only <- Seurat::RunPCA(patient_rna_only, seed.use = 1)
+  # Computes the k.param nearest neighbors
+  patient_rna_only <- Seurat::FindNeighbors(patient_rna_only, dims = patient_dims)
+  # Identify clusters of cells by a shared nearest neighbor (SNN) modularity optimization based clustering algorithm
+  # Use the resolution parameter to fine tune the number of expected clusters
+  patient_rna_only <- Seurat::FindClusters(
+    patient_rna_only,
+    resolution = patient_res,
+    random.seed = 1,
+    cluster.name = "Seurat_clusters")
+  # Uniform Manifold Approximation and Projection (UMAP) dimensional reduction technique
+  patient_rna_only <- Seurat::RunUMAP(patient_rna_only, dims = patient_dims, repulsion.strength = 5, seed.use = 1)
+  
+  return(patient_rna_only)
+  
+}
+
+####### COMPARE CLUSTERING METHODS #######
+
+compare_clustering_methods <- function(patient_rna_data) {
+  
+  # Read patient number
+  patient_num <- patient_rna_data$Patient.ID[1]
+  
+  # Create the contingency table (table used to study the correlation between the two variables)
+  contingency_tab_clusters <- table(
+    patient_rna_data$InSituType_semisup_clusters,
+    patient_rna_data$Seurat_clusters,
+    dnn = c("InSituType", "Seurat"))
+  
+  # Convert the table to a data frame for ggplot2
+  df_compare_clusters <- as.data.frame(as.table(contingency_tab_clusters))
+  df_compare_clusters$Freq <- log10(df_compare_clusters$Freq + 10)
+  
+  # Plot using ggplot2 with geom_tile
+  heatmap_seurat_vs_insitutype <- ggplot(df_compare_clusters, aes(InSituType, Seurat, fill = Freq)) +
+    geom_tile(color = "white") +
+    scale_fill_viridis_c() +
+    labs(fill = "Correlation") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(
+      title = paste("Patient", patient_num),
+      subtitle = "Seurat vs InSituType clusters"
+    )
+  
+  return(heatmap_seurat_vs_insitutype)
+}
+
+####### ANALYZE PATIENT #######
+
+analyze_patient <- function(all_patients_data, patient_num) {
+  
+  # Create directories to save patient data if they do not exist
+  patient_dir_img <- paste0(image_dir, "Patient_", patient_num, "_plots/")
+  patient_dir_rds <- paste0(rds_dir, "Patient_", patient_num, "_data/")
+  patient_dir_rds_img <- paste0(rds_dir, "Patient_", patient_num, "_data/", "Img_data/")
+  patient_directories <- list(patient_dir_img, patient_dir_rds, patient_dir_rds_img)
+  for (dir in patient_directories) {
+    if (!dir.exists(dir)) {
+      dir.create(dir, recursive = TRUE)
+    }
+  }
+  
+  # Load patient data from RDS files if they exist, otherwise generate them
+  patient_rna_rds <- paste0(rds_dir, "Patient_", patient_num, "_data/", "Patient_", patient_num, "_rna_data.rds")
+  patient_misc_rds <- paste0(rds_dir, "Patient_", patient_num, "_data/", "Patient_", patient_num, "_misc_data.rds")
+  if ((!file.exists(patient_rna_rds)) || (!file.exists(patient_misc_rds))) {
+    print("Generating patient data")
+    
+    # Extract patient data
+    all_patient_data <- extract_patient_data(all_patients_data, patient_num)
+    patient_rna_only <- all_patient_data[[1]]
+    patient_cohort <- all_patient_data[[2]]
+    patient_rna_counts <- all_patient_data[[3]]
+    patient_avg_neg_probes <- all_patient_data[[4]]
+    
+    # Normalize, scale, cluster, ...
+    patient_rna_only <- normalize_cluster_data(patient_rna_only, patient_dims = 1:25, patient_res = 0.8)
+    
+    # Get cell reference profile data from NanoString
+    # Use this reference profile as it is the only one available from CosMx data, originally from:
+    # https://raw.githubusercontent.com/Nanostring-Biostats/CosMx-Cell-Profiles/main/Human/IO/IO.profiles.csv
+    ioprofiles <- read.csv("./Cell_profile_matrices/NanoString.CosMx.Human.IO.profiles.csv", header = T, sep = ",", fill = T)
+    rownames(ioprofiles) <- ioprofiles[, 1]
+    ioprofiles <- ioprofiles[, -1] %>% as.matrix()
+    
+    # Run InSituType semisupervised clustering
+    patient_semisup <- runInSituTypeSemisupervised(patient_rna_only, ioprofiles, patient_cohort, patient_rna_counts, patient_avg_neg_probes)
+    # add phenotypes to the metadata for plotting
+    patient_rna_only$InSituType_semisup_clusters <- patient_semisup$clust
+
+    # Save the data to RDS files
+    saveRDS(patient_rna_only, file = patient_rna_rds)
+    patient_misc_data <- list(patient_cohort, patient_rna_counts, patient_avg_neg_probes)
+    saveRDS(patient_misc_data, file = patient_misc_rds)
+    
+  } else {
+    print("Reading patient data from RDS files")
+    
+    patient_rna_only <- readRDS(patient_rna_rds)
+    patient_misc_data <- readRDS(patient_misc_rds)
+    patient_cohort <- patient_misc_data[[1]]
+    patient_rna_counts <- patient_misc_data[[2]]
+    patient_avg_neg_probes <- patient_misc_data[[3]]
+  }
+  
+  # Show the significance of every principal component of the PCA
+  # It can be used to decide the number of dims of the FindNeighbors function
+  print("Generate Elbow plot")
+  elbow_plot_name <- paste0("Patient_",  patient_num, "_elbow_plot")
+  elbow_plot_rds <- paste0(patient_dir_rds_img, elbow_plot_name, ".rds")
+  if (!file.exists(elbow_plot_rds)) {
+    elbow_plot <- ElbowPlot(patient_rna_only, ndims = 50) + labs(title = paste("Patient", patient_num))
+    saveRDS(elbow_plot, file = elbow_plot_rds)
+    elbow_plot_image <- paste0(patient_dir_img, elbow_plot_name, image_ext)
+    ggsave(filename = elbow_plot_image,plot = elbow_plot)
+  } else {
+    elbow_plot <- readRDS(elbow_plot_rds)
+  }
+  
+  # Plot Mean PanCK
+  print("Generate Mean PanCK plot")
+  mean_panck_plot_name <- paste0("Patient_",  patient_num, "_panCK")
+  mean_panck_plot_rds <- paste0(patient_dir_rds_img, mean_panck_plot_name, ".rds")
+  if (!file.exists(mean_panck_plot_rds)) {
+    mean_panck_plot <- FeaturePlot(object = patient_rna_only, features = "Mean.PanCK", min.cutoff = 2000) +
+      labs(title = paste("Patient", patient_num), subtitle = "Mean PanCK")
+    saveRDS(mean_panck_plot, file = mean_panck_plot_rds)
+    mean_panck_plot_image <- paste0(patient_dir_img, mean_panck_plot_name, image_ext)
+    ggsave(filename = mean_panck_plot_image, plot = mean_panck_plot)
+  } else {
+    mean_panck_plot <- readRDS(mean_panck_plot_rds)
+  }
+  
+  # Plot KRT17
+  print("Generate KRT17 plot")
+  KRT17_plot <- FeaturePlot(
+    object = patient_rna_only,
+    features = "KRT17",
+    cols = c("white", "red")
+  ) + 
+    labs(
+      title = paste("Patient", patient_num),
+      subtitle = "KRT17"
+    )
+  ggsave(
+    filename = paste0(patient_dir_img, "Patient_",  patient_num, "_krt17", image_ext)
+  )
+  
+  ################## PRINT CLUSTERING PLOTS ##################
+  
+  # Get name of the first image
+  patient_image <- Images(patient_rna_only)[1]
+  
+  # Create a list of the clusters
+  clusters <- c("Seurat_clusters", "InSituType_semisup_clusters")
+  
+  # Save all the plots in a list to return them all together
+  clustering_plots_list <- list()
+  
+  for (cluster in clusters) {
+    
+    print(paste(cluster, "and number of cells in each of them associated with patient", patient_num))
+    print(table(patient_rna_only[[cluster]]))
+    
+    # Select the cluster as the identity
+    Idents(patient_rna_only) <- cluster
+    # Plot the cells using their polygonal boundaries
+    DefaultBoundary(patient_rna_only[[patient_image]]) <- "segmentation"
+    
+    diff_expr_genes_heatmap <- generateDynTextHeatmap(patient_rna_only, cluster)
+    
+    # Save plot to list
+    clustering_plots_list[[paste(cluster, "heatmap", sep = "_")]] <- diff_expr_genes_heatmap
+    
+    # Save the heatmap to an image
+    ggsave(
+      filename = paste0(patient_dir_img, "Patient_",  patient_num, "_", cluster, "_diff_expr_genes_heatmap", image_ext),
+      plot = diff_expr_genes_heatmap
+    )
+    
+    
+    # Graphs the output of a dimensional reduction technique on a 2D scatter plot
+    # Each point is a cell and it's positioned based on the cell embeddings determined by the reduction technique
+    umap_clusters <- Seurat::DimPlot(
+      patient_rna_only, reduction = "umap",
+      group.by = cluster,
+      label=TRUE,
+      label.box=TRUE,
+      repel=TRUE) +
+      labs(
+        title = paste("Patient", patient_num),
+        subtitle = cluster) +
+      NoLegend()
+    # Save plot to list
+    clustering_plots_list[[paste(cluster, "umap", sep = "_")]] <- umap_clusters
+    # Save plot to image file
+    ggsave(
+      filename = paste0(patient_dir_img, "Patient_",  patient_num, "_", cluster,"_umap", image_ext),
+      plot = umap_clusters
+    )
+    
+    # Plot cells in their spatial context
+    stamps_list <- list()
+    for(core in sort(unique(patient_rna_only@meta.data$core_serial))) {
+      for (stamp in sort(unique(patient_rna_only@meta.data$stamp[patient_rna_only@meta.data$core_serial == core]))) {
+        stamp_plot <- ImageDimPlot(
+          patient_rna_only,
+          fov = patient_image,
+          # Set border color to 'NA' as 'white' masks all cells when zoomed out
+          border.color = NA,
+          flip_xy = FALSE,
+          cells = row.names(patient_rna_only@meta.data)[which(patient_rna_only@meta.data$core_serial == core & patient_rna_only@meta.data$stamp == stamp)]) + theme(
+            legend.text = element_text(size = 6),
+            legend.title = element_text(size = 8),
+            legend.key.size = unit(0.5, 'lines'), # Adjust the size of the legend keys
+            legend.spacing = unit(0.5, 'lines') # Adjust the spacing between legend items
+          ) +
+          labs(
+            title = paste("Patient", patient_num, "Core", core, ", Stamp", stamp),
+            subtitle = cluster
+          )
+        clustering_plots_list[[paste(cluster, core, as.character(stamp), sep = "_")]] <- stamp_plot
+        ggsave(
+          filename = paste0(patient_dir_img, "Patient_",  patient_num, "_", cluster, "_core_", core, "_stamp_", stamp, image_ext),
+          plot = stamp_plot)
+      }
+    }
+  }
+  
+  print("Generate Comparing Clusters plot")
+  seurat_vs_insitutype_plot_name <- paste0("Patient_",  patient_num, "_heatmap_seurat_vs_insitutype")
+  seurat_vs_insitutype_plot_rds <- paste0(patient_dir_rds_img, seurat_vs_insitutype_plot_name, ".rds")
+  if (!file.exists(seurat_vs_insitutype_plot_rds)) {
+    seurat_vs_insitutype_plot <- compare_clustering_methods(patient_rna_only)
+    saveRDS(seurat_vs_insitutype_plot, file = seurat_vs_insitutype_plot_rds)
+    seurat_vs_insitutype_plot_image <- paste0(patient_dir_img, seurat_vs_insitutype_plot_name, image_ext)
+    ggsave(
+      filename = seurat_vs_insitutype_plot_image,
+      plot = seurat_vs_insitutype_plot
+    )
+  } else {
+    seurat_vs_insitutype_plot <- readRDS(seurat_vs_insitutype_plot_rds)
+  }
+  
+  # Return all plots together, otherwise only the last one is shown
+  plot_list <- c(list(elbow_plot, mean_panck_plot, KRT17_plot, seurat_vs_insitutype_plot), clustering_plots_list)
+  print(plot_list)
+  
+  return(patient_rna_only)
 }
