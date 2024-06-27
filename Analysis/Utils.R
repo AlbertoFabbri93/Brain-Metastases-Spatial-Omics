3694fade4ff944ab7a5de769eba67082be113834
Alberto Fabbri <alberto_fabbri.git@yahoo.com>
2024-06-27 15:23:44 +0200

    Work on the assay directly and preserve other assays

--- Analysis/Utils.R
+++ Analysis/Utils.R
@@ -279,16 +279,19 @@ extract_patient_data <- function(all_patients_data, patient_num) {
 
 extract_patient_rna_data <- function(patient_data, assay, start_index = 1, end_index = 1000) {
   
-  DefaultAssay(patient_data) <- assay
+  assay_to_subset <- patient_data[[assay]]
   
   # Check if the provided indices are within the valid range
-  if (start_index < 1 || end_index > nrow(patient_data) || start_index > end_index) {
+  if (start_index < 1 || end_index > nrow(assay_to_subset) || start_index > end_index) {
     stop("Invalid range of indices provided.")
   }
   
   # Create Seurat object with only the specified range of RNA data
-  features_to_include <- rownames(patient_data)[start_index:end_index]
-  patient_rna_only <- subset(x = patient_data, features = features_to_include)
+  features_to_include <- rownames(assay_to_subset)[start_index:end_index]
+  rna_only_assay <- subset(x = assay_to_subset, features = features_to_include)
+  patient_rna_only <- patient_data
+  patient_rna_only[[assay]] <- NULL
+  patient_rna_only[["RNA"]] <- rna_only_assay
   
   return(patient_rna_only)
 }
